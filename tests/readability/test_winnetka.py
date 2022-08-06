007afa411bb085d6c7bd7d6ba9a6dd3c82535b1d
Martin Freisehner <mfreisehner@gmail.com>
2022-08-06 17:51:18 +0200

    Added Winnetka scorer

--- tests/readability/test_winnetka.py
+++ tests/readability/test_winnetka.py
@@ -1,8 +1,11 @@
 import unittest
 
-from zimp.readability.winnetka import PrepositionFrequencyScore, PosMethod
+from zimp.readability.winnetka import PrepositionFrequencyScore, PosMethod, WinnetkaScore, SimpleSentenceFilter
 
 sentence_a = 'What is the average speed of the horses at the Kentucky Derby?'
+sentence_b = 'Who developed the vaccination against polio?'
+sentence_c = 'What is the name of the chocolate company in San Francisco?'
+sentence_d = 'This is an example for a non-simple sentence, but it might be a bad one.'
 
 
 class WinnetkaScoreTest(unittest.TestCase):
@@ -15,3 +18,28 @@ class WinnetkaScoreTest(unittest.TestCase):
         # same results for simple sentences - yeah!
         pfs = PrepositionFrequencyScore(pos_method=PosMethod.STANZA)
         self.assertEqual(153.84615384615387, pfs.get_score(sentence_a))
+
+    def test_simple_sentence_filter(self):
+        ssf = SimpleSentenceFilter()
+        simple_sents, _ = ssf.get_simple_sentences(sentence_a)
+        self.assertEqual(1, len(simple_sents))
+
+    def test_nonsimple_sentence_filter(self):
+        ssf = SimpleSentenceFilter()
+        simple_sents, _ = ssf.get_simple_sentences(sentence_d)
+        self.assertEqual(0, len(simple_sents))
+
+    def test_nonsimple_de_sentence_filter(self):
+        # two verbs
+        ssf = SimpleSentenceFilter(language='german')
+        simple_sents, _ = ssf.get_simple_sentences('Wer nach einem Zusammenhang sucht, findet ihn im Bekenntnis zur Abschweifung')
+        self.assertEqual(0, len(simple_sents))
+
+    def test_winnetka_sentence(self):
+        ws = WinnetkaScore()
+        self.assertEqual(860.7588461538461, ws.get_score(sentence_a))
+
+    def test_winnetka_dataset(self):
+        ws = WinnetkaScore()
+        self.assertEqual(756.6362499999999, ws.get_dataset_score([sentence_a, sentence_b, sentence_c]))
+
--- zimp/readability/winnetka.py
+++ zimp/readability/winnetka.py
@@ -1,7 +1,12 @@
 from enum import Enum
 
+import spacy.tokens
+
+from zimp.pos.countvectorizer_analyzer import CountVectorizerAnalyzer
 from zimp.pos.spacy import get_or_download_model
 from zimp.pos.stanza import get_or_download_model as get_or_download_stanza_model
+from zimp.pos.tokenization.builder import TokenizerStrategy
+from zimp.readability.lively import OutOfVocabularySizeScore
 from zimp.readability.metrics import ReadabilityScore
 
 """
@@ -14,6 +19,65 @@ The Elementary School Journal 28.5 (1928): 373-381.
 class PosMethod(Enum):
     SPACY = 0
     STANZA = 1
+
+
+class WinnetkaScore(ReadabilityScore):
+    """
+    higher score means higher difficulty; reference:
+    Score........................................... Grade
+    4–16............................................ II
+    18–34........................................... III
+    36–52........................................... IV
+    54–62........................................... V
+    64–70........................................... VI
+    72–78........................................... VII
+    80–86........................................... VIII
+    88–94........................................... IX
+    96–102.......................................... X
+    104–112......................................... XI
+    """
+
+    def __init__(
+            self,
+            language='english',
+            tokenizer_strategy: TokenizerStrategy = TokenizerStrategy.NLTK_BASE,
+            pos_method: PosMethod = PosMethod.SPACY,
+            s=1000
+    ):
+        """
+        :param language:
+        :param tokenizer_strategy: strategy used to tokenize words
+        :param pos_method: method for pos-tag extraction
+        :param s: scaling factor - inner metrics are normalized per s words
+        """
+        self.s = s
+        self.tokenizer_strategy = tokenizer_strategy
+        self.language = language
+        self.prep_extractor = self.prep_extractor = build_preposition_extractor(pos_method, language)
+        self.oov_score = OutOfVocabularySizeScore(word_tokenizer_strategy=tokenizer_strategy, language=language)
+        self.simple_sentence_filter = SimpleSentenceFilter(language=language)
+
+    def get_score(self, text: str) -> float:
+        return self.get_dataset_score([text])
+
+    def get_dataset_score(self, texts) -> float:
+        cva = CountVectorizerAnalyzer(texts, self.tokenizer_strategy, self.language)
+        word_counts = cva.extract_dataset_metric()
+        X_2 = word_counts.index.size / word_counts.sum(axis=0).iloc[0] * self.s
+        X_3 = self._get_freq_score(texts, self.prep_extractor.get_prepositions) * self.s
+        X_4 = self._get_freq_score(texts, self.oov_score.get_filtered_toks) * self.s
+        X_5 = self._get_freq_score(texts, self.simple_sentence_filter.get_simple_sentences) * 75  # fixed to base 75, see Winnetka paper
+
+        return 0.85 * X_2 + 0.101 * X_3 + 0.604 * X_4 - 0.411 * X_5 + 17.43
+
+    def _get_freq_score(self, texts, f_token_filter):
+        cnt_filter, cnt_toks = 0, 0
+        for txt in texts:
+            filtered_toks, toks = f_token_filter(txt)
+            cnt_filter += len(filtered_toks)
+            cnt_toks += len(toks)
+
+        return cnt_filter/cnt_toks
 
 
 class PrepositionFrequencyScore(ReadabilityScore):
@@ -29,13 +93,55 @@ class PrepositionFrequencyScore(ReadabilityScore):
         :param s: scaling factor s (-> number of prepositions per s words)
         """
         self.pos_method = pos_method
-        self.prep_extractor = _SpacyPrepositionExtractor(language) if pos_method == PosMethod.SPACY else \
-            _StanzaPrepositionExtractor(language)
+        self.prep_extractor = build_preposition_extractor(pos_method, language)
         self.s = s
 
     def get_score(self, text: str) -> float:
-        prep_ratio = self.prep_extractor.get_preposition_ratio(text)
+        preps, toks = self.prep_extractor.get_prepositions(text)
+        prep_ratio = len(preps) / len(toks)
         return prep_ratio * self.s
+
+
+class SimpleSentenceFilter:
+    """
+    returns sentences without dependent or co-ordinate clauses
+    approximates this by (only) checking for conjunctions-dependency tags
+    """
+
+    def __init__(self, language: str = 'english'):
+        self.base_model = get_or_download_model(language)
+        self.complex_sentence_pos_indicators = [
+            'SCONJ',  # subordinating conjunction, e.g. "I believe _that_ he will come"
+        ]
+        # dependency tags are different for german parser
+        self.subject_dep_indicators = ['sb'] if language == 'german' else ['nsubj']
+
+    def get_simple_sentences(self, text):
+        sentences = list(self.base_model(text).sents)
+        simple_sentences = [s for s in sentences if self.is_simple_sentence(s)]
+        return simple_sentences, sentences
+
+    def is_simple_sentence(self, sentence: spacy.tokens.Span):
+        complex_tokens = []
+        verb_tokens = []
+        subj_tokens = []
+
+        for idx, t in enumerate(sentence):
+            if t.pos_ in self.complex_sentence_pos_indicators and idx > 0:
+                # sconj at beginning of the sentence are not an indicator for a non-simple sentence (german)
+                complex_tokens.append(t)
+            if t.pos_ == 'VERB':
+                verb_tokens.append(t)
+            if t.dep_ in self.subject_dep_indicators:
+                subj_tokens.append(t)
+
+        # no complex sentence indicators, not more than one subject and one verb
+        return len(complex_tokens) < 1 and len(verb_tokens) < 2 and len(subj_tokens) < 2
+
+
+def build_preposition_extractor(pos_method: PosMethod, language: str):
+    return _SpacyPrepositionExtractor(language) if pos_method == PosMethod.SPACY else \
+            _StanzaPrepositionExtractor(language)
 
 
 class _SpacyPrepositionExtractor:
@@ -47,9 +153,9 @@ class _SpacyPrepositionExtractor:
     def __init__(self, language='english'):
         self.base_model = get_or_download_model(language)
 
-    def get_preposition_ratio(self, text):
+    def get_prepositions(self, text):
         toks = self.base_model(text)
-        return len([t for t in toks if t.pos_ == 'ADP']) / len(toks)
+        return [t for t in toks if t.pos_ == 'ADP'], toks
 
 
 class _StanzaPrepositionExtractor:
@@ -60,8 +166,8 @@ class _StanzaPrepositionExtractor:
     def __init__(self, language='english'):
         self.base_model = get_or_download_stanza_model(language, 'tokenize, pos')
 
-    def get_preposition_ratio(self, text):
+    def get_prepositions(self, text):
         doc = self.base_model(text)
         toks = [w for s in doc.sentences for w in s.words]
-        return len([t for t in toks if t.upos == 'ADP']) / len(toks)
+        return [t for t in toks if t.upos == 'ADP'], toks
 
